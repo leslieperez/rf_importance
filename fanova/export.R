cbbbab201a5eccfd349b690dc040e62570d4e9ef
Marcus <allbits@github.com>
2021-08-11 13:28:55 -0300

    Rewrite code for fANOVA to use new data handling.

--- fanova/export.R
+++ fanova/export.R
@@ -6,8 +6,13 @@ library(tibble)
 suppressPackageStartupMessages(library(dplyr))
 library(tidyr)
 suppressPackageStartupMessages(library(optparse))
+library(here)
 
-options = list()
+source(paste(here(),"/model_scripts/DataHandling.R",sep=""))
+
+options = list(
+    make_option(c("-t","--type"), action="store", default="perf", help="Raw performance (perf), normalized performance (norm), performance quartile (quan), normalized ranking (rank), normalized ranking with imputation (irank), ranking quartile with imputation(qrank)")
+)
 opt=parse_args(OptionParser(option_list=options),positional_arguments=T)
 if (length(opt$args)==0) {
     stop("Please give an irace data file name.\n")
@@ -16,42 +21,14 @@ datafile=opt$args[1]
 load(datafile)
 
 ## (1) write all configurations
+data = createData (configurations = iraceResults$allConfigurations,
+                   experiments = iraceResults$experiments,
+                   parameters = iraceResults$parameters,
+                   add.dummy = TRUE,
+                   add.instance=TRUE,
+                   data.type = opt$options$type)
 
-## (1.1) go over all results, collect info (parameter settings, instance, seed, value)
-opt.instance=T  ## include instance in features?
-opt.seed=F      ## include seed in features?
-
-cat("Processing",datafile,"\n")
-reslist=list()
-ri=1
-for(ii in 1:nrow(iraceResults$experiments)) {
-    ins=iraceResults$state$.irace$instancesList[ii,]
-    for(ci in 1:ncol(iraceResults$experiments)) {
-        value=iraceResults$experiments[ii,ci]
-        if (!is.na(value)) {
-            cfg=iraceResults$allConfigurations %>% filter(.ID.==ci) %>% select(-.ID.,-.PARENT.)
-            if (opt.instance) {
-                cfg$instance=ins$instance
-            }
-            if (opt.seed) {
-                cfg$seed=ins$seed
-            }
-            cfg$response=value
-            reslist[[ri]] <- cfg
-            ri = ri+1
-        }
-    }
-}
-result=bind_rows(reslist)
-
-## (1.3) code strings
-result$algorithm=as.numeric(as.factor(result$algorithm))
-result$localsearch=as.numeric(as.factor(result$localsearch))
-## "impute" NA with 10 times the domain's upper bound
-result=result %>% replace_na(list(nnls=500,q0=10,dlb=10,rasrank=1000,elitistants=7500))
-result$dlb=as.numeric(result$dlb)
-                             
-## (1.2) write fANOVA data
+## (2) write fANOVA data
 cat("Writing features and responses.\n")
-write.table(result %>% select(-response), "features.csv",row.names=F,sep=",")
-write.table(result %>% select(response), "response.csv" ,row.names=F,col.names=F,sep=",")
+write.table(data$data %>% select(-.PERFORMANCE.), file="features.csv", row.names = FALSE, sep=",", quote = FALSE)
+write.table(data$data %>% select(.PERFORMANCE.),  file="response.csv", row.names = FALSE, col.names = FALSE, sep=",", quote = FALSE)
--- fanova/process.py
+++ fanova/process.py
@@ -6,30 +6,41 @@ import pandas as pd
 from fanova import fANOVA
 import fanova.visualizer
 
-## create a config space (manually for now)
+## (1) read data
+X=pd.read_csv('features.csv')
+Y=np.loadtxt('response.csv', delimiter=",")
+
+## (2) recode categorical columns, since `low level library expects X argument to be float`
+categorical=['algorithm', 'dlb', 'instance']
+for cat in categorical:
+    X[cat]=X[cat].astype('category').cat.codes
+    
+## (3) create a config space (mostly manually for now)
 cs = ConfigSpace.ConfigurationSpace()
-cs.add_hyperparameter(hp.CategoricalHyperparameter('algorithm', choices=['0','1','2','3','4','5']))
-cs.add_hyperparameter(hp.CategoricalHyperparameter('localsearch', choices=['0','1','2','3','4']))
+cs.add_hyperparameter(hp.UniformIntegerHyperparameter('dummy', lower=1, upper=10))
+#cs.add_hyperparameter(hp.CategoricalHyperparameter('algorithm', choices=list(set(X['algorithm'].tolist()))))
+cs.add_hyperparameter(hp.UniformIntegerHyperparameter('algorithm', lower=min(X['algorithm']), upper=max(X['algorithm'])))
+cs.add_hyperparameter(hp.CategoricalHyperparameter('localsearch', choices=['0','1','2','3']))
 cs.add_hyperparameter(hp.UniformFloatHyperparameter('alpha', lower=0.0, upper=5.0))
 cs.add_hyperparameter(hp.UniformFloatHyperparameter('beta', lower=0.0, upper=10.0))
 cs.add_hyperparameter(hp.UniformFloatHyperparameter('rho', lower=0.0, upper=1.0))
 cs.add_hyperparameter(hp.UniformIntegerHyperparameter('ants', lower=5, upper=100))
-cs.add_hyperparameter(hp.UniformIntegerHyperparameter('nnls', lower=5, upper=500)) # imputed
-cs.add_hyperparameter(hp.UniformFloatHyperparameter('q0', lower=0.0, upper=10.0))  # imputed
-cs.add_hyperparameter(hp.UniformIntegerHyperparameter('dlb', lower=0, upper=10))   # imputed
-cs.add_hyperparameter(hp.UniformIntegerHyperparameter('rasrank', lower=0, upper=1000)) # imputed
-cs.add_hyperparameter(hp.UniformIntegerHyperparameter('elitistants', lower=1, upper=7500)) # imputed
-cs.add_hyperparameter(hp.UniformIntegerHyperparameter('instance', lower=0, upper=100000)) # hack
-pnames=['algorithm','localsearch','alpha','beta','rho','ants','nnls','q0','dlb','rasrank','elitistants','instance']
+cs.add_hyperparameter(hp.UniformIntegerHyperparameter('nnls', lower=5, upper=100))
+cs.add_hyperparameter(hp.UniformFloatHyperparameter('q0', lower=0.0, upper=2.0))
+#cs.add_hyperparameter(hp.CategoricalHyperparameter('dlb', choices=list(set(X['dlb'].tolist()))))
+cs.add_hyperparameter(hp.UniformIntegerHyperparameter('dlb', lower=min(X['dlb']), upper=max(X['dlb'])))
+cs.add_hyperparameter(hp.UniformIntegerHyperparameter('rasrank', lower=0, upper=200))
+cs.add_hyperparameter(hp.UniformIntegerHyperparameter('elitistants', lower=1, upper=1500))
+#cs.add_hyperparameter(hp.CategoricalHyperparameter('instance', choices=list(set(X['instance'].tolist()))))
+cs.add_hyperparameter(hp.UniformIntegerHyperparameter('instance', lower=min(X['instance']), upper=max(X['instance'])))
 
 ## execute fANOVA
-X=pd.read_csv('features.csv')
-Y = np.loadtxt('response.csv', delimiter=",")
 f = fANOVA(X,Y,config_space=cs)
 
 ## print parameter importance
+pnames=X.columns.tolist()
 with open('importance.dat','w') as inf:
-    print("parameter ind_imp tot_imp ind_std tot_std")
+    print("parameter ind_imp tot_imp ind_std tot_std",file=inf)
     for i in range(0,len(pnames)):
         imp=f.quantify_importance((pnames[i], ))[(pnames[i],)]
         print(pnames[i],imp['individual importance'],imp['total importance'],imp['individual std'],imp['total std'],file=inf)
