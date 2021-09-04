ea47e39f3f97542e747aae496b6001e97e21fa52
souzamarcelo <marcelo.desouza@udesc.br>
2021-09-04 14:10:30 -0300

    Added basic definitions code.

--- artificial_benchmarks/script.R
+++ artificial_benchmarks/script.R
@@ -2,12 +2,12 @@ source("artificial_landscape.R")
 source("importance.R")
 
 le <- Landscape$new("landscapes/parameters-conditional.txt", "landscapes/simple-conditional-landscape1.txt")
-le <- Landscape$new("landscapes/cases/param1.txt", "landscapes/cases/case1.txt")
-#le$print()
+#le <- Landscape$new("landscapes/cases/param1.txt", "landscapes/cases/case1.txt")
+le$print()
 
 
 # create configuration
-x <- c(1,2,1,1,1)
+x <- c(1,2,1,0.3,1)
 names(x) <- le$pnames
 x<- le$deactivateConfig(x)
 
