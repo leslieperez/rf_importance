aba46a6d0be5b04cb17b38b3df369e387b594c53
Marcus <allbits@github.com>
2021-09-09 10:20:29 -0300

    Fix and generalize.

--- fanova/process.py
+++ fanova/process.py
@@ -22,11 +22,13 @@ parser.add_argument('-x','--ubac',type=float,default=1.0,help='(X4) Upper bound 
 parser.add_argument('-y','--ubbd',type=float,default=1.0,help='(X4) Upper bound for B and D.')
 parser.add_argument('--tag',  type=str,default='missing',help='Tag for output.')
 parser.add_argument('--seed',type=int,default=1,help='Number of trees')
+parser.add_argument('--impute',type=float,default=2.0,help='Multiplier for imputation.')
+parser.add_argument('--sample',action='store_true',help='Sample the resulting distribution')
 args = parser.parse_args()
 
 base=os.path.basename(args.Base)
 random.seed(args.seed)
-numpy.random_seed(args.seed)
+np.random.seed(args.seed)
 
 X=pd.read_csv(args.Base+'-features.csv')
 Y=np.loadtxt(args.Base+'-response.csv', delimiter=",")
@@ -48,12 +50,14 @@ if args.configspace == 'acotsp':
     cs.add_hyperparameter(hp.UniformFloatHyperparameter('beta', lower=0.0, upper=10.0))
     cs.add_hyperparameter(hp.UniformFloatHyperparameter('rho', lower=0.0, upper=1.0))
     cs.add_hyperparameter(hp.UniformIntegerHyperparameter('ants', lower=5, upper=100))
-    cs.add_hyperparameter(hp.UniformIntegerHyperparameter('nnls', lower=5, upper=100))
-    cs.add_hyperparameter(hp.UniformFloatHyperparameter('q0', lower=0.0, upper=2.0))
-    #cs.add_hyperparameter(hp.CategoricalHyperparameter('dlb', choices=list(set(X['dlb'].tolist()))))
-    cs.add_hyperparameter(hp.UniformIntegerHyperparameter('dlb', lower=min(X['dlb']), upper=max(X['dlb'])))
-    cs.add_hyperparameter(hp.UniformIntegerHyperparameter('rasrank', lower=0, upper=200))
-    cs.add_hyperparameter(hp.UniformIntegerHyperparameter('elitistants', lower=1, upper=1500))
+    
+    cs.add_hyperparameter(hp.UniformIntegerHyperparameter('nnls', lower=5, upper=50*args.impute))                          ## imputed
+    cs.add_hyperparameter(hp.UniformFloatHyperparameter('q0', lower=0.0, upper=1.0*args.impute))                           ## imputed
+    #cs.add_hyperparameter(hp.CategoricalHyperparameter('dlb', choices=list(set(X['dlb'].tolist()))))                      ## imputed
+    cs.add_hyperparameter(hp.UniformIntegerHyperparameter('dlb', lower=min(X['dlb']), upper=max(X['dlb'])))                ## imputed
+    cs.add_hyperparameter(hp.UniformIntegerHyperparameter('rasrank', lower=0, upper=100*args.impute))                      ## imputed
+    cs.add_hyperparameter(hp.UniformIntegerHyperparameter('elitistants', lower=1, upper=750*args.impute))                  ## imputed
+    
     #cs.add_hyperparameter(hp.CategoricalHyperparameter('instance', choices=list(set(X['instance'].tolist()))))
     cs.add_hyperparameter(hp.UniformIntegerHyperparameter('instance', lower=min(X['instance']), upper=max(X['instance'])))
 elif args.configspace == 'X4':
@@ -77,11 +81,11 @@ if args.single:
     #print("Individual importance.")
     #print(base,len(X),args.ntrees,args.tag,end=' ')
     with open(base+'-importance'+args.replication+'.dat','w') as inf:
-        print("parameter ind_imp tot_imp ind_std tot_std",file=inf)
+        #print("parameter ind_imp tot_imp ind_std tot_std",file=inf)
         for i in range(0,len(pnames)):
             imp=f.quantify_importance((pnames[i], ))[(pnames[i],)]
             #print(pnames[i],imp['individual importance'],imp['total importance'],imp['individual std'],imp['total std'],file=inf)
-            print(base,replication,args.seed,len(X),args.ntrees,args.tag,pnames[i],imp['individual importance'])
+            print(base,args.replication,args.seed,len(X),args.ntrees,args.tag,pnames[i],imp['individual importance'],file=inf)
             #print(imp['individual importance'],end=' ')
     #print()
     
@@ -119,11 +123,12 @@ if args.plot:
         vis.create_all_plots()
 
 ## (8) dump a couple of predictions
-with open(base+'-samples.dat','w') as saf:
-    for i in range(0,1000):
-        (a,b)=(random.random(),1*random.random()) ## fix
-        v=f.the_forest.predict([a,b])
-        print(a,b,v,file=saf)
+if args.sample:
+    with open(base+'-samples.dat','w') as saf:
+        for i in range(0,1000):
+            (a,b)=(random.random(),1*random.random()) ## fix
+            v=f.the_forest.predict([a,b])
+            print(a,b,v,file=saf)
 
 ## (9) dump the trees
 #f.the_forest.save_latex_representation("./trees/test.tex")
